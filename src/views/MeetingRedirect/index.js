df39417406ce4acde0daf13dfdce81bb29d536c9
Nha Tran <nhatd@acb.com.vn>
2022-08-24 12:22:33 +0700

    start instant in navbar

--- src/views/MeetingRedirect/index.js
+++ src/views/MeetingRedirect/index.js
@@ -5,22 +5,22 @@ import { LIVE_MEETING_URL } from "../../configs";
 import { BrandLogoLoading, MainLayout } from "../../components";
 
 const MeetingRedirect = () => {
-  const {meetingId} = useParams();
+  const { meetingId } = useParams();
   const navigate = useNavigate();
 
   const fetchMeeting = async () => {
     try {
       const res = await checkMeetingExist(meetingId);
-      if(res && res?.meeting?.id){
+      if (res && res?.meeting?.id) {
         let url = `${LIVE_MEETING_URL}/${res?.meeting.id}`;
 
-        if(res?.meeting?.jwt){
+        if (res?.meeting?.jwt) {
           url += `?jwt=${res.meeting.jwt}`;
         }
 
         window.location = url;
-      }else{
-        navigate('/')
+      } else {
+        navigate("/");
       }
     } catch (error) {
       console.log("ScheduleMeetingDetail", error);
@@ -30,8 +30,6 @@ const MeetingRedirect = () => {
   useEffect(() => {
     fetchMeeting().then();
   }, []);
-
-
 
   return (
     <div className="h-screen">
